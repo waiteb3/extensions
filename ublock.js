0d20995c3b96cfc16a670860120caa058d0993ba
Brandon Waite <waiteb3@users.noreply.github.com>
2023-09-20 21:52:11 -0400

    fix cursor hover and wrap with try catches

--- ublock.js
+++ ublock.js
@@ -1,49 +1,63 @@
 /// OldRedditRedirect.js
-window.addEventListener("load", (event) => {
-    if (window.location.host.includes('old')) {
-        return;
-    }
+try {
+    const injectFAB = (event) => {
+        try {
+            if (window.location.host.includes('old')) {
+                return;
+            }
 
-    /**
-    .fabs {
-      --_viewport-margin: 2.5vmin;
-   
-      position: fixed;
-      z-index: var(--layer-1);
-   
-      inset-block: auto var(--_viewport-margin);
-      inset-inline: auto var(--_viewport-margin);
-    }
-    */
-    const div = document.createElement('div');
+            /**
+            .fabs {
+            --_viewport-margin: 2.5vmin;
+        
+            position: fixed;
+            z-index: var(--layer-1);
+        
+            inset-block: auto var(--_viewport-margin);
+            inset-inline: auto var(--_viewport-margin);
+            }
+            */
+            const div = document.createElement('div');
 
-    div.id = 'ublock-injected-reddit-fob';
-    div.innerText = 'Goto Old Reddit';
+            div.id = 'ublock-injected-reddit-fob';
+            div.innerText = 'Goto Old Reddit';
 
-    div.style.cssText = `
-    font-weight: bold;
-  
-    position:fixed;
-    width:10em;
-    bottom:1em;
-    right:1em;
+            div.style.cssText = `
+            font-weight: bold;
+        
+            position:fixed;
+            width:10em;
+            bottom:1em;
+            right:1em;
 
-    line-height: 2em;
-      
-    background-color:#0C9;
-    color:#FFF;
-    border-radius:50px;
-    text-align:center;
-    box-shadow: 2px 2px 3px #999;
-    
-    display: inline-block;
-    vertical-align: middle;
-    hover: cursor;
-    `;
+            line-height: 2em;
+            
+            background-color:#0C9;
+            color:#FFF;
+            border-radius:50px;
+            text-align:center;
+            box-shadow: 2px 2px 3px #999;
+            
+            display: inline-block;
+            vertical-align: middle;
+            cursor: pointer;
+            `;
 
-    document.body.appendChild(div);
+            document.body.appendChild(div);
 
-    div.onclick = () => {
-        window.location.host = 'old.reddit.com';
+            div.onclick = () => {
+                window.location.host = 'old.reddit.com';
+            };
+        } catch (err) {
+            alert(`Error while injecting FAB: ${err}`);
+        }
     };
-});
+
+    if (document.readyState === 'complete') {
+        injectFAB();
+    } else {
+        window.addEventListener("load", injectFAB);
+    }
+} catch (err) {
+    alert(`Error initing: ${err}`);
+}
