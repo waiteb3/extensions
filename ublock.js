a56fdb52261b7e82e5eee1e87aab15632e8dc385
Brandon Waite <waiteb3@users.noreply.github.com>
2023-10-24 11:13:09 -0400

    remove newlines to avoid script truncation

--- ublock.js
+++ ublock.js
@@ -5,36 +5,27 @@ try {
             if (window.location.host.includes('old')) {
                 return;
             }
-
             const div = document.createElement('myconflictfreediv');
-
             div.id = 'ublock-injected-reddit-fob';
             div.innerText = 'Goto Old Reddit';
-
             div.style.cssText = `
             font-weight: bold;
-        
             position:fixed;
             width:10em;
             bottom:1em;
             right:1em;
-
             line-height: 2em;
-            
             background-color:#0C9;
             color:#FFF;
             border-radius:50px;
             text-align:center;
             box-shadow: 2px 2px 3px #999;
-            
             display: inline-block;
             vertical-align: middle;
             cursor: pointer;
             z-index: 2147483647;
             `;
-
             document.body.appendChild(div);
-
             div.onclick = () => {
                 window.location.host = 'old.reddit.com';
             };
@@ -42,7 +33,6 @@ try {
             alert(`Error while injecting FAB: ${err}`);
         }
     };
-
     if (document.readyState === 'complete') {
         injectFAB();
     } else {
